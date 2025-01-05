bbcadc9c6e316227b14adb5d8329c6ea627c9c46
muhammadsyifaaul <muhammadsyifaaulj@gmail.com>
2025-01-05 14:35:09 +0700

    Update dependencies: immer and use-immer

--- react_dasar/src/assets/contact/ContactForn.jsx
+++ react_dasar/src/assets/contact/ContactForn.jsx
@@ -2,19 +2,35 @@ const initialData = {
     name: '',
     message: ''
 }
+import { useState } from "react"
+import { useImmer } from "use-immer"
 
 export default function ContactForn() {
-    const [contact, setContact] = useState(initialData)
+    // const [contact, setContact] = useState(initialData)
+    const [contact, setContact] = useImmer(initialData)
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setContact(draft => {
+            // ...contact,
+            // [name]: value
+            draft[name] = value;
+        })
+    }
   return (
     <div>
         <h1>Contact Form</h1>
         <form action="">
             <label htmlFor="name">Name</label>
-            <input type="text" placeholder='Masukan Nama' id='name' name='name'/><br />
+            <input type="text" placeholder='Masukan Nama' onChange={handleChange} id='name' name='name'/><br />
             <label htmlFor="message">Message</label>
-            <textarea name="message" id="message" cols="30" rows="10"></textarea><br />
+            <input type="text" placeholder='Masukan Pesan' onChange={handleChange} id='message' name='message'/>
             <button type='submit'>Submit</button>
         </form>
+        <h1>
+          Contact Details
+        </h1>
+        <p>Name: {contact.name}</p>
+        <p>Message: {contact.message}</p>
     </div>
   )
 }
--- react_dasar/src/assets/contact/Main.jsx
+++ react_dasar/src/assets/contact/Main.jsx
@@ -1,8 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import ContactForn from "./ContactForn";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        
+        <ContactForn />
     </StrictMode>
 );
